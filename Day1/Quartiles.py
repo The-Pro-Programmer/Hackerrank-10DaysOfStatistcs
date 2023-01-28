79dd22b8ea7b6797ad4f9ed4ef58bc935b75b0c7
Mansi Wakharkar <mansiswakharkar@gmail.com>
2023-01-28 18:49:25 +0300

    Day 1: Quartiles https://www.hackerrank.com/challenges/s10-quartiles/problem?isFullScreen=true

---
+++ Day1/Quartiles.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import statistics as st
+
+
+#
+# Complete the 'quartiles' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quartiles(arr):
+    op = []
+    n = len(arr)
+    arr.sort()
+    op.append(int(st.median(arr[0:n // 2])))
+    if (n % 2 == 0):
+        op.append(int(st.median(arr)))
+        op.append(int(st.median(arr[n // 2:n])))
+    else:
+        op.append(int(arr[n // 2]))
+        op.append(int(st.median(arr[1 + (n // 2):n])))
+    return op
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    data = list(map(int, input().rstrip().split()))
+
+    res = quartiles(data)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
